fcc1df61177be480b902e6897b71deead9a9ccb1
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2024-11-15 15:47:53 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,16 +7,24 @@ import router from "../routes/reviewRoute.js";
 
 const app=express();
 
-const PORT =6677;
+const PORT =6677 || process.env.PORT;
+
 app.use(express.json());
 
 app.use("/api/v1",router);
 
 
+
+app.get("/", (req, res)=>{
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
+  })
+})
+
 dotenv.config();
 
 dbConnect();
-
 
 
 
