c0eabf93f8597c5ebedf11f72358e25a79c58c9b
Chetan Pawar <163544059+chetanpawar29@users.noreply.github.com>
2024-11-15 15:29:28 +0530

    Second commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express, { response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import dbConnect from "../config/database.js";
+import User from "../model/user.js";
+import router from "../routes/reviewRoute.js";
+
+const app=express();
+
+const PORT =6677;
+app.use(express.json());
+
+app.use("/api/v1",router);
+
+
+dotenv.config();
+
+dbConnect();
+
+
+
+
+
+app.listen(PORT ,()=>{
+    console.log(`Server run at port ${PORT}`);
+})
